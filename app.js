974aa823fc753a6f1dd97c628e0aec8da34656ef
Baptiste MATHON <baptistemathon@MacBook-Pro-de-Baptiste.local>
2025-04-26 21:28:20 +0200

    adding video to cloudinary

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ mongoose.connect(process.env.MONGO_URL)
 async function uploadToCloudinary(buffer, folder){
     return new Promise((resolve, reject) => {
         const stream = cloudinary.uploader.upload_stream(
-            {folder},
+            {folder, resource_type: 'auto'},
             (error, result) => {
                 if(result) resolve(result);
                 else reject(error);
--- routes/Posts/post.js
+++ routes/Posts/post.js
@@ -1,6 +1,8 @@
 const Posts = require('../../models/posts');
 
 async function addPost(req, res) {
+    console.log('BODY:', req.body);
+    console.log('FILE:', req.file);
     const imageUrl = req.body.imageUrl || null;
     const { description, owner } = req.body;
     let tags = req.body.tags
@@ -23,6 +25,8 @@ async function addPost(req, res) {
         tags: tags || []
       });
   
+      console.log('Post à sauvegarder :', newPost);
+
       await newPost.save();
       res.status(201).json({ message: "Post created successfully", post: newPost });
     } catch (error) {
