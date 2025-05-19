bc2aa36583d0c69c6adf27fa62daf3bbe7f874fc
Baptiste MATHON <baptistemathon@MacBook-Pro-de-Baptiste.local>
2025-05-19 15:54:54 -0400

    editing ratelimiter

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,8 +1,8 @@
 const setRateLimit = require('express-rate-limit');
 
 const AnnonceRateLimiter = setRateLimit({
-    windiwMs: 1000,
-    max: 500,
+    windiwMs: 1 * 60 * 1000,
+    max: 5,
     message: 'Trop de requêtes envoyées, veuillez réessayer plus tard',
     headers: true,
 })
